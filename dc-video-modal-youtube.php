a471c772702ec71497635370daf3cc955d3dd674
charleschristopherm <74387365+charleschristopherm@users.noreply.github.com>
2024-09-17 17:11:46 -0700

    Updating source files with new changes

--- dc-video-modal-youtube.php
+++ dc-video-modal-youtube.php
@@ -22,25 +22,28 @@ $videoID = get_field( 'video_id' );
 $autoplay = get_field( 'autoplay' ) ? 1 : 0;
 $controls = get_field( 'controls' ) ? 1 : 0;
 $caption = get_field( 'caption' );
-
 ?>
-
-<figure <?php echo $anchor; ?> class="<?php echo esc_attr( $class_name ); ?>">
-    <button data-open-modal>
-	<?php echo wp_get_attachment_image( $thumbnail, 'full', '', array( 'class' => 'open-modal' ) ); ?>
+ 
+<figure <?php echo echo esc_attr( $anchor ); ?> class="<?php echo esc_attr( $class_name ); ?>">
+    <button data-open-modal="<?php echo esc_attr( $unique_id ); ?>">
+        <?php echo wp_get_attachment_image( $thumbnail, 'full', '', array( 'class' => 'open-modal' ) ); ?>
     </button>
-
-    <?php if( $caption ): ?>
+    <?php if ( $caption ): ?>
         <figcaption><?php echo $caption; ?></figcaption>
     <?php endif; ?>
 </figure>
 
-<dialog data-modal class="dc-modal">
-    <div class="iframe-container">
-        <iframe id="dc-video-iframe" width="1200" height="675" src="https://www.youtube.com/embed/<?php echo $videoID; ?>?rel=0&amp;autoplay=<?php echo $autoplay; ?>&amp;controls=<?php echo $controls; ?>&amp;enablejsapi=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+ <dialog data-modal="<?php echo esc_attr( $unique_id ); ?>" class="dc-modal">
+    <div 
+        class="iframe-container" 
+        id="iframe-container-<?php echo esc_attr( $unique_id ); ?>" 
+        data-video-id="<?php echo esc_attr( $videoID ); ?>" 
+        data-controls="<?php echo esc_attr( $controls ); ?>"
+    >
+        <!-- Iframe will be injected here dynamically via JavaScript -->
     </div>
 
     <div class="dialog-footer">
-        <button data-close-modal class="close">CLOSE</button>
+        <button data-close-modal="<?php echo esc_attr( $unique_id ); ?>" class="close">CLOSE</button>
     </div>
 </dialog>
--- video-modal-youtube.js
+++ video-modal-youtube.js
@@ -9,20 +9,45 @@
   
 	var players = {};
   
+	// Function to initialize a player for a given iframe ID
+	function initializePlayer(uniqueId) {
+		players[uniqueId] = new YT.Player(uniqueId);
+	}
+  
 	// Initialize players when API is ready
 	window.onYouTubeIframeAPIReady = function() {
-	  var iframes = document.querySelectorAll('iframe[data-youtube-iframe]');
-	  iframes.forEach(function(iframe) {
-		var uniqueId = iframe.id;
-		players[uniqueId] = new YT.Player(uniqueId);
-	  });
+	  // This needs to be here for YT API, but should be left empty as players are initialized dynamically upon iframe injection
 	};
   
 	// Function to open modal and play video
 	function openModal(uniqueId) {
 	  var modal = document.querySelector('dialog[data-modal="' + uniqueId + '"]');
+	  var iframeContainer = document.getElementById('iframe-container-' + uniqueId);
+	  var videoID = iframeContainer.getAttribute('data-video-id');
+	  var controls = iframeContainer.getAttribute('data-controls');
+  
+	  if (modal && !players[uniqueId]) {
+		// Create the iframe dynamically
+		var iframe = document.createElement('iframe');
+		iframe.id = uniqueId;
+		iframe.width = '1200';
+		iframe.height = '675';
+		iframe.src = `https://www.youtube.com/embed/${videoID}?rel=0&autoplay=1&controls=${controls}&enablejsapi=1`;
+		iframe.title = "YouTube video player";
+		iframe.frameBorder = "0";
+		iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope";
+		iframe.referrerPolicy = "strict-origin-when-cross-origin";
+		iframe.allowFullscreen = true;
+  
+		// Inject the iframe into the container
+		iframeContainer.innerHTML = '';
+		iframeContainer.appendChild(iframe);
+  
+		initializePlayer(uniqueId);
+	  }
+  
 	  if (modal) {
-		modal.showModal(); // Open the modal
+		modal.showModal();
 		var player = players[uniqueId];
 		if (player && player.playVideo) {
 		  player.playVideo();
@@ -33,8 +58,10 @@
 	// Function to close modal and pause video
 	function closeModal(uniqueId) {
 	  var modal = document.querySelector('dialog[data-modal="' + uniqueId + '"]');
+	  var iframeContainer = document.getElementById('iframe-container-' + uniqueId);
+  
 	  if (modal) {
-		modal.close(); // Close the modal
+		modal.close();
 		var player = players[uniqueId];
 		if (player && player.pauseVideo) {
 		  player.pauseVideo();
@@ -73,4 +100,4 @@
 		}
 	  });
 	});
-  })();
+})();
